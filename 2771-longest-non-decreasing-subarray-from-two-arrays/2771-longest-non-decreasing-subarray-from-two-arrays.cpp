7322e452acce8553340d2cb6e683001ca1c15dee
Sudhanva-Nadiger <93595710+Sudhanva-Nadiger@users.noreply.github.com>
2023-07-09 10:38:56 +0530

    Time: 463 ms (14.29%), Space: 193 MB (28.57%) - LeetHub

--- 2771-longest-non-decreasing-subarray-from-two-arrays/2771-longest-non-decreasing-subarray-from-two-arrays.cpp
+++ 2771-longest-non-decreasing-subarray-from-two-arrays/2771-longest-non-decreasing-subarray-from-two-arrays.cpp
@@ -1,50 +1,56 @@
-int f(int i,int flag,vector<int>&nums1,vector<int>&nums2,int n,vector<vector<int>>&dp)
-{
-    if(i == n)
-    {
-        return 0;
-    }
-    if(dp[i][flag] != -1)
-        return dp[i][flag];
-    if(flag == 0)
-    {
-        int pick1 = f(i+1,1,nums1,nums2,n,dp) + 1;
-        int pick2 = f(i+1,2,nums1,nums2,n,dp) + 1;
-        int notpick = f(i+1,0,nums1,nums2,n,dp);
-        return dp[i][flag] = max({pick1,pick2,notpick});
-    }
-    else if(flag == 1)
-    {
-        int pick1 = 0;
-        int pick2 = 0;
-        if(nums1[i-1] <= nums1[i])
-            pick1 = f(i+1,1,nums1,nums2,n,dp) + 1;
-        if(nums1[i-1] <= nums2[i])
-            pick2 = f(i+1,2,nums1,nums2,n,dp) + 1;
-        return dp[i][flag] = max(pick1,pick2);
-    }
-    else if(flag == 2)
-    {
-        int pick1 = 0;
-        int pick2 = 0;
-        if(nums2[i-1] <= nums1[i])
-            pick1 = f(i+1,1,nums1,nums2,n,dp) + 1;
-        if(nums2[i-1] <= nums2[i])
-            pick2 = f(i+1,2,nums1,nums2,n,dp) + 1;
-        return dp[i][flag] = max(pick1,pick2);
-    }
-    return dp[i][flag] = 0;
-}
-
-
-
-
 class Solution {
 public:
+    int dp[100001][3];
+    int ans = 0;
+    int f(vector<int>& a, vector<int>& b, int i, int flag) {
+        if(i >= a.size()) return 0;
+        
+        int& st = dp[i][flag];
+        if(st != -1) return st;
+        
+        if(flag == 0) {
+            int pick1 = 1+f(a, b, i+1, 1);
+            int pick2 = 1+f(a, b, i+1, 2);
+            int notPick = f(a, b, i+1, 0);
+            
+            st = max({pick1, pick2, notPick});
+            ans = max(ans, st);
+            return st;
+        }else if(flag == 1) {
+            int pick1 = 0;
+            int pick2 = 0;
+            
+            if(a[i-1] <= a[i]) {
+                pick1 = 1+f(a, b, i+1, 1);
+            }
+            if(a[i-1] <= b[i]) {
+                pick2 = 1+f(a, b, i+1, 2);
+            }
+            
+            st = max(pick1, pick2);
+            ans = max(ans, st);
+            return st;
+        }else if(flag == 2) {
+            int pick1 = 0;
+            int pick2 = 0;
+            
+            if(b[i-1] <= a[i]) {
+                pick1 = 1+f(a, b, i+1, 1);
+            }
+            if(b[i-1] <= b[i]) {
+                pick2 = 1+f(a, b, i+1, 2);
+            }
+            
+            st = max(pick1, pick2);
+            ans = max(ans, st);
+            
+            return st;
+        }
+        
+        return st = 0;
+    }
     int maxNonDecreasingLength(vector<int>& nums1, vector<int>& nums2) {
-        int flag = 0;
-        int n = nums1.size();
-        vector<vector<int>>dp(n,vector<int>(3,-1));
-        return f(0,flag,nums1,nums2,n,dp);
+        memset(dp, -1, sizeof(dp));
+        return f(nums1, nums2, 0, 0);
     }
 };
