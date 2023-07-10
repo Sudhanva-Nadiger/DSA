cd68c2c366c260052c08d31e6d047cea87909b32
Sudhanva-Nadiger <93595710+Sudhanva-Nadiger@users.noreply.github.com>
2023-07-10 12:43:38 +0530

    Time: 231 ms (78.61%), Space: 109.7 MB (79.81%) - LeetHub

--- 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.cpp
+++ 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<int> mostCompetitive(vector<int>& nums, int k) {
         vector<int> ans;
-        
-        for(int i = 0; i < nums.size(); i++) {
-            while(ans.size() && ans.back() > nums[i] && ans.size()+nums.size()-i > k) {
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            while(ans.size() && ans.back()>nums[i] && ans.size()+(n-i) > k) {
                 ans.pop_back();
             }
             
@@ -12,7 +12,6 @@ public:
                 ans.push_back(nums[i]);
             }
         }
-        
         return ans;
     }
 };
