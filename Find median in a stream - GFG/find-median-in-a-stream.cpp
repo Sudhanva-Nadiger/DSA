943afafde480d425ff10e007e0a3fd704d1bc25f
Sudhanva-Nadiger <93595710+Sudhanva-Nadiger@users.noreply.github.com>
2023-08-20 20:11:57 +0530

    Added solution - LeetHub

---
+++ Find median in a stream - GFG/find-median-in-a-stream.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    void insertHeap(int &x)
+    {
+        if(!maxHeap.size() ||x <= maxHeap.top()) {
+            maxHeap.push(x);
+        } else {
+            minHeap.push(x);
+        }
+        
+        if(maxHeap.size() > minHeap.size()+1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if(maxHeap.size() < minHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    
+    //Function to balance heaps.
+    void balanceHeaps()
+    {
+        if(maxHeap.size() > minHeap.size()+1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if(maxHeap.size() < minHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    
+    //Function to return Median.
+    double getMedian()
+    {
+        if(minHeap.size() == maxHeap.size()) {
+            return (double)(minHeap.top() + maxHeap.top())/2.0;
+        }
+        
+        return maxHeap.top();
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int n, x;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	Solution ob;
+    	cin >> n;
+    	for(int i = 1;i<= n; ++i)
+    	{
+    		cin >> x;
+    		ob.insertHeap(x);
+    	    cout << floor(ob.getMedian()) << endl;
+    	}
+    }
+	return 0;
+}
+// } Driver Code Ends
